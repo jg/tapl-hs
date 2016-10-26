acfc855a20eedade5b99d52d81ef774ebf5479f9
Juliusz Gonera <juliusz.gonera@gmail.com>
2016-10-26 20:40:46 +0200

    Subst code & specs

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -7,7 +7,20 @@ data Command =
 data Term =
   Var Info String Int Int | -- name, de brujin, context length
   Abs Info String Term |
-  App Info Term Term deriving (Eq, Show)
+  App Info Term Term deriving (Eq)
+
+class Brujin a where
+  brujin :: a -> String
+
+instance Brujin Term where
+  brujin (Var _ _ x _) = show x
+  brujin (Abs _ _ t) = "L. " ++ brujin t
+  brujin (App _ t1 t2) = brujin t1 ++ " " ++ brujin t2
+
+instance Show Term where
+  show (Var _ name x _) = "Var " ++ name ++ " " ++ show x
+  show (Abs _ name t) = "(L " ++ name ++ ". " ++ show t ++ ")"
+  show (App _ t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
 
 data Info = Info Position deriving (Eq, Show)
 
@@ -17,7 +30,6 @@ type Context = [(Name, Value)]
 
 emptyContext :: Context
 emptyContext = []
-
 
 addName :: Context -> Name -> Context
 addName ctx name = (name, "") : ctx
@@ -31,6 +43,51 @@ name2Index info ctx x =
   case ctx of
     [] -> error $ "Identifier " ++ x ++ " is unbound"
     (y, _): ys -> if x == y then 0 else 1 + (name2Index info ys x)
+
+type TermMapFn =  Info -- need to pass info to construct a new Var
+               -> String -- name of variable
+               -> Int  -- threshold above which we shift (c)
+               -> Int  -- de Brujin index (x)
+               -> Int  -- context length (n)
+               -> Term
+
+-- | Notation is arrow d c where d is the amount we want to shift, c
+-- is the integer threshold above which we actually want to shift
+tmmap :: TermMapFn
+      -> Int
+      -> Term
+      -> Term
+tmmap onvar c term =
+  let
+    walk :: Int -- de Brujin index
+         -> Term
+         -> Term
+    walk c' (Var info name x ctxlen) = onvar info name c' x ctxlen
+    walk c' (Abs info name t2) = Abs info name (walk (c' + 1) t2)
+    walk c' (App info t1 t2) = App info (walk c' t1) (walk c' t2)
+  in walk c term
+
+termShiftAbove :: Int -> Int -> Term -> Term
+termShiftAbove d c t =
+  tmmap shift c t
+  where
+    shift i name c' x n =
+        if x >= c'
+        then Var i name (x+d) (n+d)
+        else Var i name x     (n+d)
+
+termShift :: Int -> Term -> Term
+termShift d t = termShiftAbove d 0 t
+
+-- [j |-> s] t where j is de Brujin, s is a term
+termSubst :: Int -> Term -> Term -> Term
+termSubst j s t =
+  tmmap subst 0 t
+  where
+    subst i name c x n =
+        if x == j + c
+        then termShift c s
+        else Var i name x n
 
 termInfo :: Term -> Info
 termInfo (Var info _ _ _) = info
@@ -50,3 +107,4 @@ data Token =
 data Position = Position Int Int deriving (Eq, Show)
 
 data PosToken = PosToken Token Position deriving (Eq, Show)
+ 
--- test/MainSpec.hs
+++ test/MainSpec.hs
@@ -7,7 +7,14 @@ import HParser
 import Syntax
 import Control.Exception
 
-spec :: Spec
+epos = Info (Position 0 0) -- empty position
+bvar = Var epos
+v x = bvar "" x 0
+lambda = Abs epos
+l = lambda ""
+app = App epos
+ap = app
+
 spec = do
   describe "Lexer" $ do
     it "lexes single lambda" $ do
@@ -20,15 +27,59 @@ spec = do
       test "(lambda x. x) (lambda x. x x)" `shouldBe`
         [Sym '(',Lambda,Id "x",Dot,Id "x",Sym ')',Sym '(',Lambda,Id "x",Dot,Id "x",Id "x",Sym ')']
 
-  describe "Parser" $ do
+  describe "Parser (positions)" $ do
     -- it "parses a variable eval" $ do
     --   evaluate (parse (scan "x")) `shouldThrow` errorCall "Identifier x is unbound"
     it "parses a lambda" $ do
       parse (scan "lambda x.x") `shouldBe` [Eval (Info (Position 1 1)) (Abs (Info (Position 1 1)) "x" (Var (Info (Position 1 10)) "x" 0 1))] 
     it "deals with parens and application" $ do
-      parse (scan "(lambda x. x) (lambda y. y)") `shouldBe` [Eval (Info (Position 1 2)) (App (Info (Position 1 2)) (Abs (Info (Position 1 2)) "x" (Var (Info (Position 1 12)) "x" 0 1)) (Abs (Info (Position 1 16)) "y" (Var (Info (Position 1 26)) "y" 0 1)))]
+      parse (scan "(lambda x. x) (lambda x. x x)") `shouldBe` [Eval (Info (Position 1 2)) (App (Info (Position 1 2)) (Abs (Info (Position 1 2)) "x" (Var (Info (Position 1 12)) "x" 0 1)) (Abs (Info (Position 1 16)) "x" (App (Info (Position 1 26)) (Var (Info (Position 1 26)) "x" 0 1) (Var (Info (Position 1 28)) "x" 0 1))))]
     it "deals with nexted lambdas" $ do
       parse (scan "(lambda x. x (lambda y. x y))") `shouldBe` [Eval (Info (Position 1 2)) (Abs (Info (Position 1 2)) "x" (App (Info (Position 1 12)) (Var (Info (Position 1 12)) "x" 0 1) (Abs (Info (Position 1 15)) "y" (App (Info (Position 1 25)) (Var (Info (Position 1 25)) "x" 1 2) (Var (Info (Position 1 27)) "y" 0 2)))))]
     it "deals with empty input" $ do
       parse (scan "") `shouldBe` []
+
+
+  describe "termShift" $ do
+    it "shifts correctly (1)" $ do
+      let
+        -- ^2 (L. L. 1 0 2)
+        t = lambda "y" (lambda "x" (app (bvar "y" 1 2) (app (bvar "x" 0 2) (bvar "z" 2 2))))
+        in brujin (termShift 2 t) `shouldBe` "L. L. 1 0 4"
+    it "shifts correctly (2)" $ do
+      let
+        -- ^2 L. 0 1 L. 0 1 2
+        t = l (ap (v 0) (app (v 1) (l (app (v 0) (ap (v 1) (v 2))))))
+        in brujin (termShift 2 t) `shouldBe` "L. 0 3 L. 0 1 4"
+
+  describe "termSubst" $ do
+    -- [j |-> s] t
+    it "[0 |-> 1 L. 2] (0 L. 1)" $ do
+      let
+        j = 0
+        s = ap (v 1) (l (v 2))
+        t = ap (v 0) (l (v 1))
+        in brujin (termSubst j s t) `shouldBe` "1 L. 2 L. 2 L. 3"
+    it "[0 |-> 1] L. 1 0" $ do
+      let
+        j = 0
+        s = v 1
+        t = l (ap (v 1) (v 0))
+        in brujin (termSubst j s t) `shouldBe` "L. 2 0"
+    it "[0 |-> L. 1 2] L. 1" $ do
+      let
+        j = 0
+        s = l (ap (v 1) (v 2))
+        t = l (v 1)
+        in brujin (termSubst j s t) `shouldBe` "L. L. 2 3"
+    it "[0 |-> 1] 0 L. L. 2" $ do
+      let
+        j = 0
+        s = v 1
+        t = ap (v 0) (l (l (v 2)))
+        in brujin (termSubst j s t) `shouldBe` "1 L. L. 3"
         
+
+      
+
+      
